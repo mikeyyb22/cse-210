8bbb69e6dc4d6600352e12a295ea6f10f79fa115
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-02 17:02:55 -0300

    grades change from percentage to letter

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please input your current grade %: ");
+        string gradeInput = Console.ReadLine();
+        int userGrade = int.Parse(gradeInput);
+        char letterGrade = 'E';
+
+        if (userGrade >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (userGrade >= 80)
+        {
+            letterGrade = 'B';
+        }
+        else if (userGrade >= 70)
+        {
+            letterGrade = 'C';
+        }
+        else if (userGrade >= 60)
+        {
+            letterGrade = 'D';
+        }
+        else
+        {
+            letterGrade = 'F';
+        }
+        Console.WriteLine($"Your letter grade is a {letterGrade}.");
     }
 }
