44907e70411b90661e8ab42555a0cac87fc9b136
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-02 17:36:34 -0300

    created list and program that finds sum, avg, and largest num

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // List<int> numbers = new List<int>();
+        // List<string> words = new List<string>();
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+        // // Size of list
+        // Console.WriteLine(words.Count);
+        // // Iterate through list
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+
+        List<int> numbers = new List<int>();
+        bool inputCheck = false;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (inputCheck == false)
+        {
+            Console.Write("Enter number: ");
+            string userEntry = Console.ReadLine();
+            int userInput = int.Parse(userEntry);
+            if (userInput == 0)
+            {
+                inputCheck = true;
+            }
+            else
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        int total = numbers.Count;
+        float average = sum / total;
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNum = 0;
+        foreach (int num in numbers)
+        {
+            if (num > largestNum)
+            {
+                largestNum = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNum}");
     }
 }
