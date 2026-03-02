3d520c6f80b348bc1fbae84858f98dd666341286
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-02 17:14:34 -0300

    created number guessing program

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        bool guessCorrect = false;
+
+        while (guessCorrect == false)
+        {
+            Console.Write("What is the magic number? (1-100) ");
+            string userGuess = Console.ReadLine();
+            int userInt = int.Parse(userGuess);
+
+            if (userInt == number)
+            {
+                Console.WriteLine("Nice job! You guessed the magic number!");
+                guessCorrect = true;
+            }
+            else if (userInt > number)
+            {
+                Console.WriteLine("Lower...");
+            }
+            else
+            {
+                Console.WriteLine("Higher...");
+            }
+        }
+        
     }
 }
