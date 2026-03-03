424fca2101de6f805c46fcb6ef53b0255feb2535
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-03 11:02:22 -0300

    created functions to square numbers

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        //Functions
+        // returnType FunctionName(dataType parameter1, dataType parameter2)
+        // {
+        //     // function_body
+        // }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squaredNum = SquareNumber(userNum);
+        DisplayResult(userName, squaredNum);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int userNum = int.Parse(userInput);
+
+        return userNum;
+    }
+
+    static int SquareNumber(int favNum)
+    {
+        int squareNum = favNum * favNum;
+        return squareNum;
+    }
+
+    static void DisplayResult(string userName, int userNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {userNum}");
     }
 }
