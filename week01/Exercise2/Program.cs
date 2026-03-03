059d207b5dcbfe99f9562911bb634a7b658220ba
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-03 14:30:05 -0300

    added pass/fail message

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -7,19 +7,23 @@ class Program
         Console.Write("Please input your current grade %: ");
         string gradeInput = Console.ReadLine();
         int userGrade = int.Parse(gradeInput);
+        bool passedClass = false;
         char letterGrade = 'E';
 
         if (userGrade >= 90)
         {
             letterGrade = 'A';
+            passedClass = true;
         }
         else if (userGrade >= 80)
         {
             letterGrade = 'B';
+            passedClass = true;
         }
         else if (userGrade >= 70)
         {
             letterGrade = 'C';
+            passedClass = true;
         }
         else if (userGrade >= 60)
         {
@@ -30,5 +34,13 @@ class Program
             letterGrade = 'F';
         }
         Console.WriteLine($"Your letter grade is a {letterGrade}.");
+        if (passedClass == true)
+        {
+            Console.WriteLine("You passed! Nice job!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Try again, you can do it!");
+        }
     }
 }
