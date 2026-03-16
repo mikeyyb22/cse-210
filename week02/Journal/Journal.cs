bb72b873351a60670b6a4e8bc2054283f59412da
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-16 17:28:10 -0300

    separate csv with pipes

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -28,7 +28,7 @@ class Journal
         {
             foreach (Entry entry in _entries)
             {
-                outputFile.WriteLine($"{entry._date}~{entry._promptText}~{entry._entryText}");
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
             }
         }
     }
@@ -39,7 +39,7 @@ class Journal
 
         foreach (string line in lines)
         {
-            string[] parts = line.Split("~");
+            string[] parts = line.Split("|");
             Entry entry = new Entry();
             entry._date = parts[0];
             entry._promptText = parts[1];
