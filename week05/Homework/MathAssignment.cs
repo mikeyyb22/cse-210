7fdfcfa3959cd039c9a9872254b21ed20dbd3144
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-29 12:24:07 -0300

    homework assignment - inheritance

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,16 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string textBookSection, string problems, string studentName, string topic) : base(studentName, topic)
+    {
+        _textbookSection = textBookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} - Problems {_problems}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -6,5 +6,11 @@ class Program
     {
         Assignment assignment1 = new Assignment("Bob Jones", "Multiplication");
         Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("5.9", "6-25", "Bub Dubbs", "Calculus");
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment history1 = new WritingAssignment("The Things Our Fathers Saw", "Bib Dibbs", "History");
+        Console.WriteLine(history1.GetWritingInformation());
     }
 }
