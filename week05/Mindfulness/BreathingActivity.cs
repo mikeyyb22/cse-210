ebd2b6f35a73bad66b2c68af6a977a2bcd482b8f
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-30 12:52:12 -0300

    added Console.Clear() to breathing activity

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -13,12 +13,16 @@ public class BreathingActivity : Activity
         int numItems = GetDuration() / 10;
 
         // Show series of messages alternating between "Breathe in..." and "Breathe out..." pausing several seconds between, showing a countdown
-        for (int i = 0; i <= numItems; i++)
+        for (int i = 0; i < numItems; i++)
         {
+            Console.Clear();
             Console.Write("Breathe In...");
             ShowCountdown(5);
+            Console.Clear();
             Console.Write("Breathe Out...");
             ShowCountdown(5);
         }
+
+        Console.Clear();
     }
 }
