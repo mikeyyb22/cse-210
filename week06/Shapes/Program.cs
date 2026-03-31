fd6e1dc9b26a9e9621ae4df91fc3c9e0ae869a82
MMcGehee <mcgeheemichael22@gmail.com>
2026-03-31 18:18:44 -0300

    shapes exercise

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new Square(25, "red");
+        Rectangle rec1 = new Rectangle(7, 10, "blue");
+        Circle circle1 = new Circle(6.5, "green");
+
+        List<Shape> shapesList = new List<Shape>();
+        shapesList.Add(square1);
+        shapesList.Add(rec1);
+        shapesList.Add(circle1);
+
+        foreach (Shape sh in shapesList)
+        {
+            double area = sh.GetArea();
+            Console.WriteLine($"Area is {area}.");
+        }
+
     }
 }
